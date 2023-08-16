94d64452fcb7cccf12f2047b04366c3616460a75
tamasubi <hidtamas@gmail.com>
2023-08-16 17:16:21 +0200

    cart total works

--- store/views.py
+++ store/views.py
@@ -15,8 +15,9 @@ def cart(request):
 	else:
 		#Create empty cart for now for non-logged in user
 		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
 
-	context = {'items':items}
+	context = {'items':items, 'order':order}
 	return render(request, 'store/cart.html', context)
 
 def checkout(request):
